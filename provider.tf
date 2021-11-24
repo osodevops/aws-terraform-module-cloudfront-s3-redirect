d72ee280e189eadb322865fc9fa062fb549e8ba2
Sion Smith <sion.smith@gmail.com>
2021-11-24 20:26:29 +0000

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,12 +1,14 @@
 provider "aws" {
   alias   = "main"
-  version = "~> 2.9"
+  region  = "eu-west-2"
+  version = "~> 3.8.0"
   # description = "AWS Region for S3 and other resources"
 }
 
 provider "aws" {
+  region = "us-east-1"
   alias   = "cloudfront"
-  version = "~> 2.9"
+  version = "~> 3.8.0"
   # description = "AWS Region for Cloudfront (ACM certs only supports us-east-1)"
 }
 
